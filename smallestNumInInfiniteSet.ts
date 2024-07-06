ba240cadd9408e67cf2d29aefe5f9738c6191bd5
Timeo Williams <timwillie73@gmail.com>
2024-07-06 15:12:06 -0400

    #2336: smallest num in inifinite set. added vitest testing

---
+++ smallestNumInInfiniteSet.ts
@@ -0,0 +1,38 @@
+export class SmallestInfiniteSet {
+  smallest: number;
+  available: number[];
+  constructor() {
+      this.smallest = 1;
+      this.available = [];
+  }
+      // this makes sense
+  popSmallest(): number {
+      const small = Math.min(this.smallest, this.available[0] || Infinity);
+      if(small === this.smallest) {
+          this.smallest ++;
+      }
+      if(small === this.available[0]) {
+          this.available.shift();
+      }
+      return small;
+  }
+
+  addBack(num: number): void {
+      if(num < this.smallest && !this.available.includes(num)) {
+          this.available.push(num);
+          this.available.sort((a,b) => a - b);
+      }
+  }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * var obj = new SmallestInfiniteSet()
+ * var param_1 = obj.popSmallest()
+ * obj.addBack(num)
+ */
+
+const smallestInfiniteSet = new SmallestInfiniteSet();
+smallestInfiniteSet.available = [1,2,3,4,5];
+
+
