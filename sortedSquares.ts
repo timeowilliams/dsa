9002c87be53e44ce1114362ae879c81c40d4f1bc
Timeo Williams <timwillie73@gmail.com>
2024-08-20 23:25:36 -0400

    feat: 977 Squares of a Sorted Array)

---
+++ sortedSquares.ts
@@ -0,0 +1,22 @@
+/*
+977. Squares of a Sorted Array
+Time Complexity: O (N Log N)
+Space Complexity: O(N)
+*/
+
+export function sortedSquares(nums: number[]): number[] {
+
+  const sortAndSquareArr = []
+
+  // 1st step: square all the elements into a new array ()
+  for(let i = 0; i < nums.length; i += 1) {
+    sortAndSquareArr.push(nums[i] * nums[i]);
+  }
+
+  // 2nd step: Sort the elements in ascending order. 
+  sortAndSquareArr.sort((a,b) => a - b)
+
+  // return the new array.
+  return sortAndSquareArr;
+}
+
