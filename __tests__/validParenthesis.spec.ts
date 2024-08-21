6f12aaff6038d936eac4cb31df004c34ffc8f033
Timeo Williams <timwillie73@gmail.com>
2024-08-21 11:57:40 -0400

    feat: 20. Valid Parentheses

--- __tests__/validParenthesis.spec.ts
+++ __tests__/validParenthesis.spec.ts
@@ -1,2 +1,10 @@
 import { expect, test } from "vitest";
 import { isValid } from "../validParenthesis";
+
+test('Expect Input of "()[]{}" to be true', () => {
+  expect(isValid("()[]{}")).toBe(true);
+});
+
+test('Expect Input of "(]" to be true', () => {
+  expect(isValid("(}")).toBe(false);
+});
