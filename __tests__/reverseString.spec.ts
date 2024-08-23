fd70a2a79d2a04a410077d9043b6c928d7ed02c5
Timeo Williams <timwillie73@gmail.com>
2024-08-22 21:52:45 -0400

    feat: #344. Reverse String

---
+++ __tests__/reverseString.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "vitest";
+import { reverseString } from "../reverseString";
+
+test('Expect ["h","e","l","l","o"] to be ["o","l","l","e","h"]', () => {
+  expect(reverseString(["h", "e", "l", "l", "o"])).toEqual([
+    "o",
+    "l",
+    "l",
+    "e",
+    "h",
+  ]);
+});
+
+test('Expect ["H","a","n","n","a","h"] to be ["h","a","n","n","a","H"]', () => {
+  expect(reverseString(["H", "a", "n", "n", "a", "h"])).toEqual([
+    "h",
+    "a",
+    "n",
+    "n",
+    "a",
+    "H",
+  ]);
+});
---
+++ reverseString.ts
@@ -0,0 +1,30 @@
+/*
+Problem: 344. Reverse String
+Time Complexity: 
+Space Complexity:
+
+*/
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+export function reverseString(s: string[]): string[] {
+  // to do this while being in O(1) memory. You use two pointers and
+  // iterate up until the half way point of the string
+  // 'abcde' > 'abc' (with c being the pivot) | 'abcdef'
+  //  s[0] = s[4], s[1]=s[3], s[2]   ||  s[0] =s[5], s[1] == s[4], s[2]=s[3],
+  // so we can do a comparison for the very last right index. As long as the left < right, keep doing operations
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+
+    left += 1;
+    right -= 1;
+  }
+  return s;
+}
